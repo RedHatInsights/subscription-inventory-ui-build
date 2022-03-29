06d027a38d053bdb9d04eed70f40923f74260e5c
Travis CI User <travis@example.org>
2022-03-29 12:52:59 +0000

    Show empty state when no search results are found (#6)

--- js/412.d3e9943e7311b2f2c836.js
+++ js/412.d3e9943e7311b2f2c836.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunksubscription_inventory=self.webpackChunksubscription_inventory||[]).push([[412],{8412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>l});var r=n(2950),o=n.n(r),a=n(334),c=n(3476),s=n(319),i=n(9116),u=n(5240);const l=function(e){var t=e.logger;return o().createElement(c.Provider,{store:(t?(0,s.S1)(t):(0,s.S1)()).getStore()},o().createElement(a.BrowserRouter,{basename:"".concat((0,u.eb)(window.location.pathname,3))},o().createElement(i.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.564f27472df48bf8e795.js.map
+//# sourceMappingURL=../sourcemaps/412.d0f87be879d2aa378530.js.map
