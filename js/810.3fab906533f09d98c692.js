7d0fd0e0847b3464bf6f649282f3512c11d3973d
Travis CI User <travis@example.org>
2022-04-18 16:12:05 +0000

    TEAMNADO-3379 - Add getting started resources

--- js/810.3fab906533f09d98c692.js
+++ js/810.3fab906533f09d98c692.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunksubscription_inventory=self.webpackChunksubscription_inventory||[]).push([[810],{2810:(n,e,t)=>{t.r(e),t.d(e,{default:()=>r});var l=t(2950),u=t.n(l),i=t(334),o=t(6350),c=t(167);const r=(0,i.withRouter)((function(){return(0,l.useEffect)((function(){var n,e,t;null===(n=insights)||void 0===n||null===(e=n.chrome)||void 0===e||null===(t=e.appAction)||void 0===t||t.call(e,"oops-page")}),[]),u().createElement(o.Z,null,u().createElement(c.Z,null))}))}}]);
-//# sourceMappingURL=../sourcemaps/810.03fe0a6ef03357ec4029.js.map
+//# sourceMappingURL=../sourcemaps/810.528caab7d29939a0ff82.js.map
