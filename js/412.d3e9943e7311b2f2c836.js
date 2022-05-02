a880e824225682daf67ce811a9c83bcee1a8dbf8
Travis CI User <travis@example.org>
2022-05-02 13:32:42 +0000

    Merge pull request #10 from RedHatInsights/TEAMNADO-3381
    TEAMNADO-3381 - Add SKU to table

--- js/412.d3e9943e7311b2f2c836.js
+++ js/412.d3e9943e7311b2f2c836.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunksubscription_inventory=self.webpackChunksubscription_inventory||[]).push([[412],{8412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>l});var r=n(2950),o=n.n(r),a=n(334),c=n(3476),s=n(319),i=n(9116),u=n(5240);const l=function(e){var t=e.logger;return o().createElement(c.Provider,{store:(t?(0,s.S1)(t):(0,s.S1)()).getStore()},o().createElement(a.BrowserRouter,{basename:"".concat((0,u.eb)(window.location.pathname,3))},o().createElement(i.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.528caab7d29939a0ff82.js.map
+//# sourceMappingURL=../sourcemaps/412.3341ea527e6f2ffa1854.js.map
