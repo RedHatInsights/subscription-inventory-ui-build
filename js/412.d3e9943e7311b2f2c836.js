62a4142bd467f5a4d0fb6ff7f81200e0865de6a0
Travis CI User <travis@example.org>
2022-05-16 20:23:08 +0000

    Merge pull request #12 from RedHatInsights/TEAMNADO-3403
    Changed 'Service Level' to 'Service level' to match wireframe

--- js/412.d3e9943e7311b2f2c836.js
+++ js/412.d3e9943e7311b2f2c836.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunksubscription_inventory=self.webpackChunksubscription_inventory||[]).push([[412],{8412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>l});var r=n(2950),o=n.n(r),a=n(334),c=n(3476),s=n(319),i=n(9116),u=n(5240);const l=function(e){var t=e.logger;return o().createElement(c.Provider,{store:(t?(0,s.S1)(t):(0,s.S1)()).getStore()},o().createElement(a.BrowserRouter,{basename:"".concat((0,u.eb)(window.location.pathname,3))},o().createElement(i.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.0ffd92c699dbe5665f93.js.map
+//# sourceMappingURL=../sourcemaps/412.9ed29c36842986bad1a2.js.map
