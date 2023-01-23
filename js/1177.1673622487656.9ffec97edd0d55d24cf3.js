b7f397d6a08506937e41304626358bde000850a3
Travis CI User <travis@example.org>
2023-01-23 18:58:27 +0000

    Merge pull request #123 from RedHatInsights/TEAMNADO-4578
    Promote Teamnado 4578 to prod-beta

--- js/1177.1673622487656.9ffec97edd0d55d24cf3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunksubscription_inventory=self.webpackChunksubscription_inventory||[]).push([[1177],{1177:()=>{}}]);
--- js/2857.1673622487656.9ffec97edd0d55d24cf3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunksubscription_inventory=self.webpackChunksubscription_inventory||[]).push([[2857],{2857:()=>{}}]);
